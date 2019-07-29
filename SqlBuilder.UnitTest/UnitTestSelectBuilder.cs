19c1b0ff2ec372d8131f8525050100189d93cdc2
Viktor Török <torokv@vnet.hu>
2019-07-29 10:03:29 +0200

    Create SelectBuilder class Add unit tests

---
+++ SqlBuilder.UnitTest/UnitTestSelectBuilder.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SqlBuilder.UnitTest
+{
+    [TestClass]
+    public class UnitTestSelectBuilder
+    {
+        [TestMethod]
+        public void EmptyConstructor()
+        {
+            SelectBuilder select = new SelectBuilder();
+
+            Assert.AreEqual(select.SelectStatement, string.Empty);
+        }
+
+        [TestMethod]
+        public void NotEmptyConstructor()
+        {
+            string sqlTest = "select * from table";
+
+            SelectBuilder select = new SelectBuilder(sqlTest);
+
+            Assert.AreEqual(select.SelectStatement, sqlTest);
+        }
+
+        [TestMethod]
+        public void EmptyWhere()
+        {
+            string sqlTest = "select * from table";
+
+            SelectBuilder select = new SelectBuilder(sqlTest);
+
+            Assert.IsFalse(select.SelectStatement.Contains("where"));
+        }
+
+        [TestMethod]
+        public void WhereWithOneColumn()
+        {
+            string sqlTest = "select * from table";
+
+            SelectBuilder select = new SelectBuilder(sqlTest);
+            select.AddCondition("column1 = :column1");
+
+            Assert.IsTrue(select.SelectStatement.Contains("where"));
+            Assert.IsTrue(select.SelectStatement.Contains("column1 = :column1"));
+        }
+
+        [TestMethod]
+        public void WhereWithMoreColumns()
+        {
+            string sqlTest = "select * from table";
+
+            SelectBuilder select = new SelectBuilder(sqlTest);
+            select.AddCondition("column1 = :column1");
+            select.AddCondition("column2 = :column2");
+
+            Assert.IsTrue(select.SelectStatement.Contains("where"));
+            Assert.IsTrue(select.SelectStatement.Contains("column1 = :column1"));
+            Assert.IsTrue(select.SelectStatement.Contains("and column2 = :column2"));
+        }
+
+        [TestMethod]
+        public void EmptyOrderBy()
+        {
+            string sqlTest = "select * from table";
+
+            SelectBuilder select = new SelectBuilder(sqlTest);
+
+            Assert.IsFalse(select.SelectStatement.Contains("order by"));
+        }
+
+        [TestMethod]
+        public void OrderByWithOneColumn()
+        {
+            string sqlTest = "select * from table";
+
+            SelectBuilder select = new SelectBuilder(sqlTest);
+            select.AddOrderBy("column1");
+
+            Assert.IsTrue(select.SelectStatement.Contains("order by"));
+            Assert.IsTrue(select.SelectStatement.EndsWith("order by column1"));
+        }
+
+        [TestMethod]
+        public void OrderByWithMoreColumns()
+        {
+            string sqlTest = "select * from table";
+
+            SelectBuilder select = new SelectBuilder(sqlTest);
+            select.AddOrderBy("column1");
+            select.AddOrderBy("column2");
+
+            Assert.IsTrue(select.SelectStatement.Contains("order by"));
+            Assert.IsTrue(select.SelectStatement.EndsWith("order by column1, column2"));
+        }
+    }
+}
---
+++ SqlBuilder/SelectBuilder.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlBuilder
+{
+    public class SelectBuilder
+    {
+        #region Private fields
+        private StringBuilder sqlBuilder;
+        private StringBuilder sqlWhere;
+        private StringBuilder sqlOrderBy;
+        #endregion
+
+        #region Properties
+        public string SelectStatement
+        {
+            get
+            {
+                return this.GetSelectStatement();
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public SelectBuilder() : this(string.Empty)
+        {
+        }
+
+        public SelectBuilder(string sqlStatement)
+        {
+            sqlBuilder = new StringBuilder(sqlStatement);
+            sqlWhere = new StringBuilder();
+            sqlOrderBy = new StringBuilder();
+        }
+        #endregion
+
+        public void AddCondition(string condition)
+        {
+            if (sqlWhere.Length > 0)
+            {
+                sqlWhere.AppendLine();
+                sqlWhere.Append("   and ");
+            }
+
+            sqlWhere.Append(condition);
+        }
+        
+        public void AddCondition(TextBox textBox, string condition)
+        {
+            if (!string.IsNullOrEmpty(textBox.Text))
+            {
+                this.AddCondition(condition);
+            }
+        }
+
+        public void AddCondition(CheckBox checkBox, string condition)
+        {
+            if (checkBox.Checked)
+            {
+                this.AddCondition(condition);
+            }
+        }
+
+        public void AddOrderBy(string orderBy)
+        {
+            if (sqlOrderBy.Length > 0)
+            {
+                sqlOrderBy.Append(", ");
+            }
+
+            sqlOrderBy.Append(orderBy);
+        }
+
+        private string GetSelectStatement()
+        {
+            StringBuilder selectStatement = new StringBuilder(sqlBuilder.ToString());
+
+            if (sqlWhere.Length > 0)
+            {
+                selectStatement.AppendLine();
+                selectStatement.Append(" where ");
+                selectStatement.Append(sqlWhere.ToString());
+            }
+
+            if (sqlOrderBy.Length > 0)
+            {
+                selectStatement.AppendLine();
+                selectStatement.Append(" order by ");
+                selectStatement.Append(sqlOrderBy.ToString());
+            }
+
+            return selectStatement.ToString();
+        }
+
+        public override string ToString()
+        {
+            return GetSelectStatement();
+        }
+    }
+}
